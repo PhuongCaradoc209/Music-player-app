40bd44f949a8b8ab71678d229cd67dee85595716
= <phuongnguyen20904@gmail.com>
2024-08-18 16:03:51 +0700

    Add recommend album part in mid box

--- Client/JS/homePage.js
+++ Client/JS/homePage.js
@@ -1,13 +1,53 @@
-function scroll_left(){
-    var left = document.querySelector(".library_box-header-cover-scroll");
-    left.scrollBy({ 
-            left: -150,
-            behavior: 'smooth' });
+document.addEventListener('DOMContentLoaded', function () {
+
+    setUp();
+
+    document.getElementById("main_heading-cover-button_1").addEventListener('click', function () {
+        document.getElementById("mid_box_main").classList.add('hide');
+        document.getElementById("mid_box_searching").classList.add('show');
+    });
+
+    document.getElementById("main_heading-cover-button_2").addEventListener('click', function () {
+        document.getElementById("mid_box_main").classList.remove('hide');
+        document.getElementById("mid_box_searching").classList.remove('show');
+    });
+});
+
+function setUp() {
+    var username = document.getElementById("username").textContent;
+    var mid_box_main_p2_title = document.getElementById("mid_box_main_p2_title");
+    mid_box_main_p2_title.innerHTML = "For " + username;
 }
 
-function scroll_right(){
+
+function scroll_left() {
+    var left = document.querySelector(".library_box-header-cover-scroll");
+    left.scrollBy({
+        left: -150,
+        behavior: 'smooth'
+    });
+}
+
+function scroll_right() {
     var right = document.querySelector(".library_box-header-cover-scroll");
-    right.scrollBy({ 
-            left: 150,
-            behavior: 'smooth' });
+    right.scrollBy({
+        left: 150,
+        behavior: 'smooth'
+    });
+}
+
+function scroll_left_main_p1() {
+    var left = document.querySelector(".mid_box_main_p1_scroll");
+    left.scrollBy({
+        left: -758,
+        behavior: 'smooth'
+    });
+}
+
+function scroll_right_main_p1() {
+    var right = document.querySelector(".mid_box_main_p1_scroll");
+    right.scrollBy({
+        left: 758,
+        behavior: 'smooth'
+    });
 }
